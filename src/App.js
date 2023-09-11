a611eb894822e60c48435aed460305e4b7972c72
Alishba Siddique <alishbasiddique07@gmail.com>
2023-09-11 12:51:08 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageGenerator from './Components/ImageGenerator/ImageGenerator.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ImageGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,83 @@
+import React, { useRef, useState } from 'react';
+import './ImageGenerator.css';
+import default_image from '../Assets/default_image_1.svg';
+
+const ImageGenerator = () => {
+  const [image_url, setImage_url] = useState('/');
+  let inputRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const imageGenerator = async () => {
+    if (inputRef.current.value === '') {
+      return 0;
+    }
+    setLoading(true);
+    const response = await fetch(
+      'https://api.openai.com/v1/images/generations',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${REACT_APP_OPENAI_API_KEY}`,
+          'User-Agent': 'Edge',
+        },
+        body: JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n: 1,
+          size: '512x512',
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      console.error(`API request failed with status ${response.status}`);
+      return;
+    }
+
+    let data = await response.json();
+
+    console.log('API response:', data);
+
+    if (!data || !data.data || data.data.length === 0) {
+      console.error('API response does not contain expected data.');
+      return;
+    }
+    let data_array = data.data;
+    setImage_url(data_array[0].url);
+    setLoading(false);
+  };
+
+  return (
+    <div className="ai-image-generator">
+      <div className="header">
+        AI Image <span>Generator</span>
+      </div>
+      <div className="img-loading">
+        <img src={image_url === '/' ? default_image : image_url} alt="" />
+        <div className="loading">
+          <div className={loading ? 'loading-bar-full' : 'loading-bar'}></div>
+          <div className={loading ? 'loading-text' : 'display-none'}>
+            Loading...
+          </div>
+        </div>
+      </div>
+      <div className="search-box">
+        <input
+          type="text"
+          ref={inputRef}
+          className="search-input"
+          placeholder="Describe What You Want to See?"
+        />
+        <div
+          className="generate-btn"
+          onClick={() => {
+            imageGenerator();
+          }}
+        >
+          Generate
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageGenerator;
